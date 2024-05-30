27a68984fb4088a39b6b57f11851951200316cba
patramanik <patramanik409@gmail.com>
2024-05-30 10:56:59 +0530

    first commit

---
+++ src/components/Tables/Table.tsx
@@ -0,0 +1,9 @@
+const Table = () => {
+    return(
+        <div className="m-10 rounded-sm border border-stroke bg-white shadow-default dark:border-strokedark dark:bg-boxdark">
+            <h1>hello</h1>
+        </div>
+    );
+};
+
+export default Table;
