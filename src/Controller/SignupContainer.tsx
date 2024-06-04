61b8202bb14ac6042cedf796ec931d97d53a5bf7
saniyaj mallik <saniyajmallik12@gmail.com>
2024-06-04 09:30:44 +0530

    redux-setup2

--- src/Controller/SignupContainer.tsx
+++ src/Controller/SignupContainer.tsx
@@ -1,20 +1,21 @@
 // containers/SignupContainer.tsx
 import React from 'react';
 import SignupForm from '../pages/Authentication/SignUp.tsx';
-import User from '../models/User.ts';
-import { signup } from '../services/authService.ts';
+// import User from '../models/User.ts';
+// import { signup } from '../services/authService.ts';
 
 const SignupContainer: React.FC = () => {
-  const handleSignup = async (user: User) => {
-    try {
-      await signup(user);
-      alert('Signup successful!');
-    } catch (error) {
-      alert('Signup failed. Please try again.');
-    }
-  };
+  // const handleSignup = async (user: User) => {
+  //   try {
+  //     await signup(user);
+  //     alert('Signup successful!');
+  //   } catch (error) {
+  //     alert('Signup failed. Please try again.');
+  //   }
+  // };
 
-  return <SignupForm onSubmit={handleSignup} />;
+  // return <SignupForm onSubmit={handleSignup} />;
+  return <SignupForm />;
 };
 
 export default SignupContainer;
